d67f6131cdd5d0416bf7a49940a3ef932a24419d
Quill Lu <ql800831@gmail.com>
2024-12-29 22:55:48 +0800

    產生目標球體

--- app.js
+++ app.js
@@ -1,43 +1,34 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const unit = 30;
+let ballArray = [];
 const row = canvas.height / unit; // 320 / 20 = 16
 const column = canvas.width / unit; // 320 / 20 = 16
 //劃出棒子 讓棒子跟著滑鼠走
 
+function getRandomArbitrary(min, max) {
+  return min + Math.floor(Math.random() * (max - min));
+}
 class Ball {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.width = 50;
+    this.height = 50;
+    ballArray.push(this);
+    this.visible = true;
+  }
+
+  drawBall() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
-// class Ball {
-//   constructor() {
-//     this.x = Math.floor(Math.random() * row) * unit;
-//     this.y = Math.floor(Math.random() * column) * unit;
-//     this.hasDrawn = false;
-//   }
-
-//   drawBall() {
-//     console.log(this.hasDrawn);
-//     // 如果已經執行過，直接返回
-//     if (this.hasDrawn) return;
-//     // 設定新的隨機位置
-//     let new_x = Math.floor(Math.random() * row) * unit;
-//     let new_y = Math.floor(Math.random() * column) * unit;
-
-//     // 繪製球
-//     ctx.fillStyle = "red";
-//     ctx.fillRect(new_x, new_y, unit, unit);
-
-//     // 打印位置到控制台
-//     console.log(`果实位置: x=${new_x}, y=${new_y}`);
-
-//     // 設置標誌為 true，表示已執行過
-//     this.hasDrawn = true;
-//   }
-// }
+for (let i = 0; i < 10; i++) {
+  new Ball(getRandomArbitrary(0, 950), getRandomArbitrary(0, 550));
+  console.log(ballArray);
+}
 
 let myBall = new Ball();
 let mouseX;
@@ -52,10 +43,14 @@ function draw() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  myBall.drawBall();
   //劃出棒子 跟著滑鼠的X座標走
   ctx.fillStyle = "yellow";
   ctx.fillRect(mouseX - 50, 500, 100, 20);
+
+  //畫出球體
+  ballArray.forEach((ball) => {
+    ball.drawBall();
+  });
 }
 
 // canvas.addEventListener("mousemove", draw);
